cdc7ecfc9fb0c2673f8cbabacdce5d3df94b0238
LilianCharlotte <Lilian.krieg@live.de>
2022-12-21 00:27:16 +0100

    Add styling for jobPostingCard with mui

--- frontend/src/component/JobPostingCard.tsx
+++ frontend/src/component/JobPostingCard.tsx
@@ -1,4 +1,5 @@
 import {JobPosting} from "../model/JobPosting";
+import {Box, Button, Card, CardActions, CardContent, Typography} from "@mui/material";
 
 export type JobPostingCardProps = {
     jobPosting: JobPosting
@@ -7,12 +8,29 @@ export type JobPostingCardProps = {
 export default function JobPostingCard(props: JobPostingCardProps) {
 
 
-    return (<div>
-        JobPosting
-        <p>{props.jobPosting.jobPostingLink}</p>
-        <p>{props.jobPosting.jobDescription}</p>
-        <p>Work from home: {props.jobPosting.isRemote? "yes" : "no"} </p>
-    </div>)
+    return (<Box m={2}>
+        <Card sx={{ minWidth: 275, maxWidth: 300 }}>
+        <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" textAlign="left" gutterBottom>
+           JobPosting
+            </Typography>
+            <Typography variant="h5" component="div">
+                {props.jobPosting.companyName}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {!props.jobPosting.isUnsolicited? props.jobPosting.jobTitle : "write an unsolicited application"}
+            </Typography>
+            <Typography variant="body2">
+                <a href={props.jobPosting.jobPostingLink}>{props.jobPosting.jobPostingLink}</a> <br/>
+                located in: {props.jobPosting.locatedAt}<br/>
+                work from home: {props.jobPosting.isRemote? "yes" : "no"}<br/>
+            </Typography>
+            <CardActions>
+                <Button size="small">View</Button>
+                <Button size="small">Move</Button>
+            </CardActions>
 
-
+        </CardContent>
+        </Card>
+    </Box>)
 }
