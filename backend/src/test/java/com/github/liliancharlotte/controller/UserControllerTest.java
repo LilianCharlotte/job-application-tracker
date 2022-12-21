d9ca53ef284090aa270849218891bd69961aa0cd
LilianCharlotte <Lilian.krieg@live.de>
2022-12-21 15:52:10 +0100

    Add test for getUserById and rename variables

--- backend/src/test/java/com/github/liliancharlotte/controller/UserControllerTest.java
+++ backend/src/test/java/com/github/liliancharlotte/controller/UserControllerTest.java
@@ -12,8 +12,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -59,11 +62,28 @@ class UserControllerTest {
                 .getResponse()
                 .getContentAsString();
 
-        User resultUser = objectMapper.readValue(response, User.class);
-        assertEquals("test", resultUser.name());
+        User actualUser = objectMapper.readValue(response, User.class);
+        assertEquals("test", actualUser.name());
         JobPosting expectedJobPosting = new JobPosting("12345", "testCompany", true, "", "", "testCompany.com", false, "Berlin");
-        assertEquals(expectedJobPosting, resultUser.jobPostings().get(0));
+        assertEquals(expectedJobPosting, actualUser.jobPostings().get(0));
     }
+
+    @Test
+    void getUserByIdAndExpectStatusToBeOk() throws Exception {
+        User expectedUser = new User("13", "test", new ArrayList<>());
+        userRepo.save(expectedUser);
+
+        String response = mockMvc.perform(get(userEndPoint+"/13"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        User actualUser = objectMapper.readValue(response, User.class);
+        assertEquals(expectedUser, actualUser);
+    }
+
+
 
 
 }
