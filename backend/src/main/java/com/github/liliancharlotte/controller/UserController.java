eb16a8d67d16b7d6c27c7bb633c086bb7e650b48
LilianCharlotte <Lilian.krieg@live.de>
2022-12-21 18:53:52 +0100

    Change apiCalls and remove autowired annotation

--- backend/src/main/java/com/github/liliancharlotte/controller/UserController.java
+++ backend/src/main/java/com/github/liliancharlotte/controller/UserController.java
@@ -3,29 +3,28 @@ package com.github.liliancharlotte.controller;
 import com.github.liliancharlotte.model.User;
 import com.github.liliancharlotte.model.UserRequest;
 import com.github.liliancharlotte.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
 
-    @Autowired
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public Optional<User> getUserById(@PathVariable String id) {
         return userService.findById(id);
     }
 
-    @PostMapping("/user")
+    @PostMapping()
     public User addUser(@RequestBody UserRequest userRequest) {
         return userService.addUser(userRequest);
     }
