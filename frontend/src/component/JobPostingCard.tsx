3d1db486e494fb170cfde94d99c2e0b1d82f13fb
LilianCharlotte <Lilian.krieg@live.de>
2023-01-03 21:04:39 +0100

    Adjust maximum card size to make columns fit

--- frontend/src/component/JobPostingCard.tsx
+++ frontend/src/component/JobPostingCard.tsx
@@ -9,18 +9,18 @@ export default function JobPostingCard(props: JobPostingCardProps) {
 
 
     return (
-        <Card sx={{m: '1rem', minWidth: 275, maxWidth: 300}}>
+        <Card sx={{m: '0.5rem', maxWidth: 245, maxHeight: 185}}>
             <CardContent>
-                <Typography sx={{fontSize: 14}} color="text.secondary" textAlign="left" gutterBottom>
+                <Typography sx={{fontSize: 10}} color="text.secondary" textAlign="left" gutterBottom>
                     JobPosting
                 </Typography>
-                <Typography variant="h5" component="div">
+                <Typography sx={{fontSize: 18}} variant="h5" component="div">
                     {props.jobPosting.companyName}
                 </Typography>
-                <Typography sx={{mb: 1.5}} color="text.secondary">
+                <Typography sx={{mb: 1.5, fontSize: 13}} color="text.secondary">
                     {!props.jobPosting.isUnsolicited ? props.jobPosting.jobTitle : "write an unsolicited application"}
                 </Typography>
-                <Typography variant="body2">
+                <Typography sx={{fontSize: 11}} variant="body2">
                     <a href={props.jobPosting.jobPostingLink}>{props.jobPosting.jobPostingLink}</a> <br/>
                     located in: {props.jobPosting.locatedAt}<br/>
                     working remotely: {props.jobPosting.isRemote ? "yes" : "no"}<br/>
