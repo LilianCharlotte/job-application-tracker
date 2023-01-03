335c066568a03a58e0438ac5c143a8d452b52a83
LilianCharlotte <Lilian.krieg@live.de>
2023-01-03 21:40:44 +0100

    Add column status frontend

--- frontend/src/model/JobPosting.ts
+++ frontend/src/model/JobPosting.ts
@@ -1,12 +1,17 @@
+export enum ColumnStatus {
+    INTERESTED_IN = "INTERESTED_IN",
+    CURRENTLY_WORKING_ON = "CURRENTLY_WORKING_ON"
+}
 
 export type JobPosting = {
-    id : string,
-    userId : string,
-    companyName : string,
-    isUnsolicited : boolean,
-    jobTitle : string,
-    jobDescription : string,
-    jobPostingLink : string,
-    isRemote : boolean,
-    locatedAt : string
+    id: string,
+    userId: string,
+    companyName: string,
+    isUnsolicited: boolean,
+    jobTitle: string,
+    jobDescription: string,
+    jobPostingLink: string,
+    isRemote: boolean,
+    locatedAt: string,
+    status: ColumnStatus
 }
