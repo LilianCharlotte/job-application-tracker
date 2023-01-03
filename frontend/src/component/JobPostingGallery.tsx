af6872b6d5feade5680ba0bf7f87b2d563e8ef51
LilianCharlotte <Lilian.krieg@live.de>
2023-01-03 23:15:55 +0100

    Add sorting cards in according columns

--- frontend/src/component/JobPostingGallery.tsx
+++ frontend/src/component/JobPostingGallery.tsx
@@ -1,5 +1,6 @@
 import {User} from "../model/User";
 import JobPostingCard from "./JobPostingCard";
+import {Grid} from "@mui/material";
 
 export type JobPostingGalleryProps = {
     user: User
@@ -8,9 +9,21 @@ export type JobPostingGalleryProps = {
 export default function JobPostingGallery(props: JobPostingGalleryProps) {
 
     return <div>
-        {props.user?.jobPostings.length !== 0 && props.user.jobPostings.map(
-            jobPosting => <JobPostingCard jobPosting={jobPosting} key={jobPosting.id}/>)
-        }
+
+        <Grid container spacing={2}>
+            <Grid item>
+                1
+                {props.user?.jobPostings.length !== 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "INTERESTED_IN").map(
+                    filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting} key={filteredJobPosting.id}/>)
+                }
+            </Grid>
+            <Grid item>
+                2
+                {props.user?.jobPostings.length !== 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "CURRENTLY_WORKING_ON").map(
+                    filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting} key={filteredJobPosting.id}/>)
+                }
+            </Grid>
+        </Grid>
     </div>
 
 }
