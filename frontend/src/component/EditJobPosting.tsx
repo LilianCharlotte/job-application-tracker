e8da12ce7fb80a410b8c61e6682e5f08bd383cbe
LilianCharlotte <Lilian.krieg@live.de>
2023-01-20 16:16:33 +0100

    Add input fields for edit component

--- frontend/src/component/EditJobPosting.tsx
+++ frontend/src/component/EditJobPosting.tsx
@@ -1,14 +1,86 @@
-import {JobPostingRequest} from "../model/JobPosting";
-import {useLocation} from "react-router-dom";
-import {Box, Button, Card, CardContent, Grid, TextField, Typography} from "@mui/material";
+import {JobPosting, JobPostingRequest, WorkModel} from "../model/JobPosting";
+import {useLocation, useNavigate} from "react-router-dom";
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Grid,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography
+} from "@mui/material";
 import {theme} from "../App";
+import {ChangeEvent, MouseEvent, useState} from "react";
 
 type EditJobPostingProps = {
     handleEditJobPosting(jobPostingId: string, jobPostingRequest: JobPostingRequest): void
 }
 export default function EditJobPosting(props: EditJobPostingProps) {
+    const navigate = useNavigate();
     const {state} = useLocation();
     const {jobPosting} = state;
+    const [editedJobPosting, setEditedJobPosting] = useState<JobPosting>(jobPosting);
+
+    function onSaveEditedJobPosting(event: MouseEvent<HTMLButtonElement>) {
+        event.preventDefault();
+        const editedJobPostingRequest = {
+            companyName: editedJobPosting.companyName,
+            isUnsolicited: editedJobPosting.isUnsolicited,
+            jobTitle: editedJobPosting.jobTitle,
+            jobDescription: editedJobPosting.jobDescription,
+            jobPostingLink: editedJobPosting.jobPostingLink,
+            remote: editedJobPosting.remote,
+            locatedAt: editedJobPosting.locatedAt,
+            status: editedJobPosting.status
+        }
+        props.handleEditJobPosting(jobPosting.id, editedJobPostingRequest);
+        navigate("/");
+    }
+
+    function handleChangeJobPostingCompanyName(event: ChangeEvent<HTMLInputElement>) {
+        event.preventDefault()
+        setEditedJobPosting(editedJobPosting => ({
+            ...editedJobPosting,
+            companyName: event.target.value
+        }))
+    }
+
+    function handleChangeJobPostingLocation(event: ChangeEvent<HTMLInputElement>) {
+        event.preventDefault()
+        setEditedJobPosting(editedJobPosting => ({
+            ...editedJobPosting,
+            locatedAt: event.target.value
+        }))
+    }
+
+    function handleChangeJobTitle(event: ChangeEvent<HTMLInputElement>) {
+        event.preventDefault()
+        setEditedJobPosting(editedJobPosting => ({
+            ...editedJobPosting,
+            jobTitle: event.target.value
+        }))
+    }
+
+    function handleChangeJobDescription(event: ChangeEvent<HTMLInputElement>) {
+        event.preventDefault()
+        setEditedJobPosting(editedJobPosting => ({
+            ...editedJobPosting,
+            jobDescription: event.target.value
+        }))
+    }
+
+    function handleChangeRemote(event: ChangeEvent<HTMLInputElement>) {
+        event.preventDefault()
+        setEditedJobPosting(editedJobPosting => ({
+            ...editedJobPosting,
+            remote: event.target.value as WorkModel
+        }))
+    }
 
     return (<Box sx={{display: 'flex', justifyContent: 'center', width: '100%'}}>
         <Card sx={{display: 'flex', flexDirection: 'row', padding: '2rem'}}>
@@ -17,7 +89,7 @@ export default function EditJobPosting(props: EditJobPostingProps) {
                     <Box
                         component="form"
                         sx={{
-                            '& .MuiTextField-root': {m: 1, width: '43ch'},
+                            '& .MuiTextField-root': {m: 1, width: '43ch'}
                         }}
                         noValidate
                         autoComplete="off"
@@ -35,36 +107,62 @@ export default function EditJobPosting(props: EditJobPostingProps) {
 
                             <Grid item xs={6}>
                                 <TextField
-                                    id="outlined-helperText"
+                                    id="company name"
                                     label={"Company name"}
-                                    defaultValue={jobPosting.companyName}
-                                    value={jobPosting.companyName}
+                                    value={editedJobPosting.companyName}
+                                    onChange={handleChangeJobPostingCompanyName}
                                 /> </Grid>
 
                             <Grid item xs={6}>
                                 <TextField
-                                    id="outlined-helperText"
-                                    label={"Job title"}
-                                    defaultValue={jobPosting.jobTitle}
-                                    value={jobPosting.jobTitle}
+                                    id="location"
+                                    label={"City or region"}
+                                    value={editedJobPosting.locatedAt}
+                                    onChange={handleChangeJobPostingLocation}
                                 /> </Grid>
 
-                            <Grid item xs={6}>
-                                <TextField
-                                    id="outlined-helperText"
-                                    label={"Job description"}
-                                    defaultValue={jobPosting.jobDescription}
-                                    value={jobPosting.jobDescription}
-                                    multiline
-                                    rows={3}
-                                /> </Grid>
+                            {!editedJobPosting.isUnsolicited ? (<>
+                                <Grid item xs={6}>
+                                    <TextField
+                                        id="job title"
+                                        label={"Job title"}
+                                        value={editedJobPosting.jobTitle}
+                                        onChange={handleChangeJobTitle}
+                                    /> </Grid>
+
+                                <Grid item xs={6}>
+                                    <TextField
+                                        id="job description"
+                                        label={"Job description"}
+                                        value={editedJobPosting.jobDescription}
+                                        onChange={handleChangeJobDescription}
+                                        multiline
+                                        rows={3}
+                                    /> </Grid>
+
+                                <Grid item xs={6}>
+                                    <FormControl sx={{ml: '0.7rem'}}>
+                                        <FormLabel id="demo-radio-buttons-group-label">Working models</FormLabel>
+                                        <RadioGroup
+                                            name="radio-buttons-group"
+                                            value={editedJobPosting.remote}
+                                            onChange={handleChangeRemote}
+                                        >
+                                            <FormControlLabel value="IN_OFFICE" control={<Radio/>}
+                                                              label="Office only"/>
+                                            <FormControlLabel value="REMOTE" control={<Radio/>} label="Remote"/>
+                                            <FormControlLabel value="HYBRID" control={<Radio/>} label="Hybrid"/>
+                                        </RadioGroup>
+                                    </FormControl>
+                                </Grid>
+                            </>) : ""}
 
                         </div>
                     </Box>
                 </Grid>
                 <Box sx={{display: 'flex', justifyContent: 'right', mr: '2rem', mt: '1.5rem'}}>
                     <Button variant="contained" sx={{m: '0.2rem'}}>Discard changes</Button>
-                    <Button variant="contained" sx={{m: '0.2rem'}}>
+                    <Button variant="contained" sx={{m: '0.2rem'}} onClick={onSaveEditedJobPosting}>
                         Save changes</Button>
                 </Box>
             </CardContent>
