25562f4aeb2e685afc939399104d2569624526c8
LilianCharlotte <Lilian.krieg@live.de>
2023-01-21 18:43:49 +0100

    Add unit test for line coverage

--- backend/src/test/java/com/github/liliancharlotte/service/UserServiceTest.java
+++ backend/src/test/java/com/github/liliancharlotte/service/UserServiceTest.java
@@ -1,6 +1,6 @@
 package com.github.liliancharlotte.service;
 
-import com.github.liliancharlotte.model.User;
+import com.github.liliancharlotte.model.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,5 +25,15 @@ class UserServiceTest {
     void whenUpdateUserWithIdThatDoesNotExistThenThrowException() {
         User testDummy = new User("2", "Dummy", new ArrayList<>());
         assertThrows(NoSuchElementException.class, () -> userService.updateUser("1234", testDummy));
+    }
+
+    @Test
+    void whenEditJobPostingInUserWithJobPostingIdThatDoesNotExistThenThrowException() {
+        UserRequest userRequest = new UserRequest("dummy", new ArrayList<>());
+        User testDummy = userService.addUser(userRequest);
+        JobPostingRequest jobPostingRequest = new JobPostingRequest("editedTestCompanyName", true, "", "", "editedLinkTestCompany.com", WorkModel.IN_OFFICE, "Hamburg", ColumnStatus.INTERESTED_IN);
+
+        String userId = testDummy.id();
+        assertThrows(NoSuchElementException.class, () -> userService.editJobPostingInUser(userId, "904", jobPostingRequest));
     }
 }
