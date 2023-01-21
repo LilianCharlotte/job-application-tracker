14d5331017891caacacad25898ad63f8bdf77a9e
LilianCharlotte <Lilian.krieg@live.de>
2023-01-22 00:20:38 +0100

    Add other column statuses in backend

--- backend/src/main/java/com/github/liliancharlotte/model/ColumnStatus.java
+++ backend/src/main/java/com/github/liliancharlotte/model/ColumnStatus.java
@@ -2,5 +2,8 @@ package com.github.liliancharlotte.model;
 
 public enum ColumnStatus {
     INTERESTED_IN,
-    CURRENTLY_WORKING_ON
+    CURRENTLY_WORKING_ON,
+    APPLICATION_SUBMITTED,
+    INTERVIEW_SCHEDULED,
+    DECIDING_ON_JOB_OFFERING
 }
