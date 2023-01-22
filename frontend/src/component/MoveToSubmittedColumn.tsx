51adeb076d75e9b49909eaed9b1682dffee239d4
LilianCharlotte <Lilian.krieg@live.de>
2023-01-23 00:48:13 +0100

    Add styling for first draft of editor and dateTimePicker

--- frontend/src/component/MoveToSubmittedColumn.tsx
+++ frontend/src/component/MoveToSubmittedColumn.tsx
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import {Paper, TextField, Typography} from "@mui/material";
+import {Button, Grid, Paper, TextField, Typography} from "@mui/material";
 import {DateTimePicker, LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import {Dayjs} from "dayjs";
+import {theme} from "../App";
 
 
 export default function MoveToSubmittedColumn() {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState(`
+        <h3>Notes</h3>
+        <br/> <br/> <br/>
+        <h3>Questions</h3>
+    `);
     const [date, setDate] = useState<Dayjs | null>();
 
     function dateToString() {
@@ -19,24 +24,53 @@ export default function MoveToSubmittedColumn() {
 
     }
 
-    return <Paper sx={{padding: "50px", alignItems: 'center'}}>
-        <Typography variant={'h2'} sx={{alignItems: 'center'}}>Test</Typography>
-        <div style={{display: "flex", alignItems: 'center'}}>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateTimePicker
-                    renderInput={(props) => <TextField {...props} />}
-                    label="DateTimePicker"
-                    value={date}
-                    inputFormat="DD.MM.YYYY HH:mm"
-                    onChange={(newValue) => {
-                        setDate(newValue);
-                    }}
-                />
-            </LocalizationProvider>
-            <div style={{backgroundColor: 'white', color: "black", width: '550px', height: '300px', margin: "auto"}}>
-                <ReactQuill theme="snow" value={value} onChange={setValue} style={{height: '258px'}}/>
-            </div>
-        </div>
+    return <Paper
+        sx={{display: "flex", alignItems: 'center', flexDirection: 'column', maxWidth: '620px', margin: 'auto'}}>
+        <Typography variant="h3" gutterBottom
+                    sx={{
+                        color: theme.palette.primary.main,
+                        p: '1rem',
+                        ml: '1rem',
+                        textAlign: 'left',
+                        width: '100%',
+                        fontSize: '3rem'
+                    }}>
+            Move job posting to<br/> 'Application submitted'
+        </Typography>
+        <Grid container>
+            <Grid item xs={5} sx={{m: '1rem', width: '100%', fontSize: '1.2rem'}}>
+                When did you submit your application?
+            </Grid>
+            <Grid item xs={5} sx={{m: '1rem'}}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateTimePicker
+                        renderInput={(props) => <TextField {...props} />}
+                        label="Date of submission"
+                        value={date}
+                        inputFormat="DD.MM.YYYY HH:mm"
+                        onChange={(newValue) => {
+                            setDate(newValue);
+                        }}
+                    />
+                </LocalizationProvider>
+            </Grid>
 
-    </Paper>
+            <Grid item xs={7} sx={{m: '1rem'}}>
+                Some space for notes about the company and for questions for your future interview:
+            </Grid>
+
+            <Grid item xs={12} sx={{m: '1rem'}}>
+                <div style={{
+                    backgroundColor: 'white',
+                    color: "black",
+                    width: '550px',
+                    height: '300px',
+                    margin: "auto"
+                }}>
+                    <ReactQuill theme="snow" value={value} onChange={setValue} style={{height: '258px'}}/>
+                </div>
+            </Grid>
+        </Grid>
+        <Button variant="contained" sx={{m: '1rem'}}>
+            Save</Button> </Paper>
 }
