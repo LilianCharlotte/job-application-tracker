09108c51a313c9500f742ef3da9e007225ff3eeb
LilianCharlotte <Lilian.krieg@live.de>
2023-01-22 01:30:12 +0100

    Add logic for moving jobPostings to other columns in frontend

--- frontend/src/component/JobPostingGallery.tsx
+++ frontend/src/component/JobPostingGallery.tsx
@@ -8,24 +8,51 @@ export type JobPostingGalleryProps = {
 
 export default function JobPostingGallery(props: JobPostingGalleryProps) {
 
-    return <Grid container spacing={2} sx={{color: 'white'}}>
-            <Grid item>
-                <Box>
-                    Interested in
-                    {props.user?.jobPostings.length > 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "INTERESTED_IN").map(
-                        filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting}
-                                                              key={filteredJobPosting.id}/>)
-                    }
-                </Box>
-            </Grid>
-            <Grid item>
-                <Box>
-                    Currently working on
-                    {props.user?.jobPostings.length > 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "CURRENTLY_WORKING_ON").map(
-                        filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting}
-                                                              key={filteredJobPosting.id}/>)
-                    }
-                </Box>
-            </Grid>
+    return <Grid container spacing={1} sx={{color: 'white'}}>
+        <Grid item xs={2.4}>
+            <Box>
+                Interested in
+                {props.user?.jobPostings.length > 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "INTERESTED_IN").map(
+                    filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting}
+                                                          key={filteredJobPosting.id}/>)
+                }
+            </Box>
         </Grid>
+        <Grid item xs={2.4}>
+            <Box>
+                Currently working on
+                {props.user?.jobPostings.length > 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "CURRENTLY_WORKING_ON").map(
+                    filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting}
+                                                          key={filteredJobPosting.id}/>)
+                }
+            </Box>
+        </Grid>
+        <Grid item xs={2.4}>
+            <Box>
+                Application submitted
+                {props.user?.jobPostings.length > 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "APPLICATION_SUBMITTED").map(
+                    filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting}
+                                                          key={filteredJobPosting.id}/>)
+                }
+            </Box>
+        </Grid>
+        <Grid item xs={2.4}>
+            <Box>
+                Interview scheduled
+                {props.user?.jobPostings.length > 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "INTERVIEW_SCHEDULED").map(
+                    filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting}
+                                                          key={filteredJobPosting.id}/>)
+                }
+            </Box>
+        </Grid>
+        <Grid item xs={2.4}>
+            <Box>
+                Deciding on job offering
+                {props.user?.jobPostings.length > 0 && props.user.jobPostings.filter(jobPosting => jobPosting.status === "DECIDING_ON_JOB_OFFERING").map(
+                    filteredJobPosting => <JobPostingCard jobPosting={filteredJobPosting}
+                                                          key={filteredJobPosting.id}/>)
+                }
+            </Box>
+        </Grid>
+    </Grid>
 }
