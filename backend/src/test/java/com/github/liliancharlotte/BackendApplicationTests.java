e0235e2f0a0547411906acce6ba929a7372cbf1f
LilianCharlotte <Lilian.krieg@live.de>
2023-01-29 23:59:29 +0100

    Remove empty test

--- backend/src/test/java/com/github/liliancharlotte/BackendApplicationTests.java
+++ backend/src/test/java/com/github/liliancharlotte/BackendApplicationTests.java
@@ -3,19 +3,11 @@ package com.github.liliancharlotte;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void testMain() {
-        BackendApplication.main(new String[]{});
-        assertTrue(true);
     }
 
 }
